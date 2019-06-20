2ae7b55cd526e2e03d5e71ff4380c1f5c19a02c8
Isidor Brkic <igi.brkic@mail.utoronto.ca>
2019-06-20 10:12:05 -0400

    Testing to call rw functions

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,12 @@
 package app;
 
+import com.xilinx.rapidwright.util.MessageGenerator;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
+
+        MessageGenerator.briefMessage("Hello RapidWright");
+        
     }
 }
